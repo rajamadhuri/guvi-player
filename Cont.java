6280a2ddaad0a3cf2d344db3fdef42fee0e39897
rajamadhuri <madhurimurugesapandian@gmail.com>
2016-09-04 22:03:28 +0530

    Create Cont.java

---
+++ Cont.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+pubic class Cont
+{
+public static void main(String args[])
+{
+String parent="";
+String search="";
+BufferedReader br=new BufferedReader();
+parent=br.nextLine();
+search=br.nextLine();
+if(parent.contains(search))
+{
+System.out.println("The string contains:".search);
+}
+else
+{
+System.out.println("The string does not contain:".search);
+}
+}
+}
