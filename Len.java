1c8c65ee56e8cea73b02eb387fcb0c3cf87a6cc1
rajamadhuri <madhurimurugesapandian@gmail.com>
2016-09-05 11:49:45 +0530

    Create Len.java

---
+++ Len.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Len
+{
+public static void main(String args[])
+{
+String str1;
+BufferedReader br=new BuffereReader();
+str1=br.nextLine();
+System.out.println("String length:".str1.length());
+}
+}
